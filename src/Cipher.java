07efdf7754815cfecc754db0bfe6a2efc3e0f8bc
Rinkan <git@github.com:Rinkanz/FirstProject.git>
2025-01-10 22:56:47 +0300

    Добавил символ ':'

--- src/Cipher.java
+++ src/Cipher.java
@@ -20,7 +20,7 @@ public class Cipher {
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 
-            '.', ',', '-', ':', ' ', ';', '!', '\n');
+            '.', ',', '-', ':', ' ', ';', '!', '\n',':');
 
 
     public static void encrypted() throws IOException {
